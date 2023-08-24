afee0feeb2dd9fe2d29422f5f426c9c0bdc4e413
Andy Wang <andywangpt@gmail.com>
2023-08-24 16:49:00 -0700

    decent first layout

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -10,30 +10,41 @@ import { GRID_DATA } from './buttonData.js';
 
 export function HomeScreen() {
 
-     const buttonStyle = {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-        margin: 1, // adjust as needed
-      height: 70,
-     };
+   const buttonStyle = {
+      justifyContent: 'center',
+      alignItems: 'center',
+      margin: 0,
+      padding: 0,
+      height: 75,
+      backgroundColor: 'darkgray',
+      width: '8.18%'
+   };
+   
+   const displayStyle = {
+      justifyContent: 'center',
+      alignItems: 'center',
+      height: 75,
+      backgroundColor: 'lightgrey',
+      margin: 2,
+      width: '57%'
+  };
    
    return (
      
-   <YStack f={1} jc="flex-start" ai="center" p="$4" space>
-      <XStack w="100%" space="$2" ai="center">
-         <Button><Text>🏠</Text></Button>
-         <Button><Text>🔍</Text></Button> 
-         <Paragraph w="7/12" ta="center" bc="grey">Display Text Here</Paragraph>
-         <Button><Text>⬅️</Text></Button>
-         <Button><Text>X</Text></Button> 
-         <Button><Text>🏠</Text></Button>
+   <YStack f={1} jc="flex-start" ai="center" p="$1" space="$1">
+      <XStack w="100%" space="$1" jc="space-between" ai="center">
+         <Button style={buttonStyle}><Text>🏠</Text></Button>
+         <Button style={buttonStyle}><Text>🔍</Text></Button> 
+         <Paragraph style={displayStyle}>Display Text Here</Paragraph>
+         <Button style={buttonStyle}><Text>⬅️</Text></Button>
+         <Button style={buttonStyle}><Text>X</Text></Button> 
+         <Button style={buttonStyle}><Text>🏠</Text></Button>
         </XStack>
 
    
   {/* Grid */}
       {GRID_DATA.map((row, rowIndex) => (
-        <XStack key={rowIndex} w="100%" space="$2" ai="center">
+        <XStack key={rowIndex} w="100%" space="$1" ai="center">
           {row.map((btn, colIndex) => (
             <Button key={colIndex} style={[buttonStyle, { backgroundColor: btn.color }]}>
               <Text>{btn.word}</Text>
