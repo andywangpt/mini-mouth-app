b42b279b5e3573686a87c55491440238f6a1c27f
Andy Wang <andywangpt@gmail.com>
2023-08-24 17:30:00 -0700

    speech working

--- packages/app/features/home/screen.tsx
+++ packages/app/features/home/screen.tsx
@@ -7,9 +7,32 @@ import {
 import React, { useState } from 'react'
 import { Text } from 'react-native'
 import { GRID_DATA } from './buttonData.js';
+import * as Speech from 'expo-speech';
 
 export function HomeScreen() {
 
+   const [displayText, setDisplayText] = useState('');   
+
+   const handleWordButtonPress = (word) => {
+      setDisplayText(prevText => {
+         return prevText + ' ' + word;
+      });
+      Speech.speak(word);
+   };
+
+   const deleteLastWord = () => {
+      setDisplayText((prevText) => {
+      const words = prevText.trim().split(' ');
+      if (words.length <= 1) return ''; // If only one word or no words, return an empty string.
+         words.pop(); // Remove the last word.
+         return words.join(' '); // Convert the array of words back into a string.
+      });
+   };
+
+   const clearDisplayText = () => {
+      setDisplayText('');
+   };
+   
    const buttonStyle = {
       justifyContent: 'center',
       alignItems: 'center',
@@ -28,30 +51,35 @@ export function HomeScreen() {
       margin: 2,
       width: '57%'
   };
-   
-   return (
-     
-   <YStack f={1} jc="flex-start" ai="center" p="$1" space="$1">
-      <XStack w="100%" space="$1" jc="space-between" ai="center">
-         <Button style={buttonStyle}><Text>🏠</Text></Button>
-         <Button style={buttonStyle}><Text>🔍</Text></Button> 
-         <Paragraph style={displayStyle}>Display Text Here</Paragraph>
-         <Button style={buttonStyle}><Text>⬅️</Text></Button>
-         <Button style={buttonStyle}><Text>X</Text></Button> 
-         <Button style={buttonStyle}><Text>🏠</Text></Button>
-        </XStack>
 
-   
-  {/* Grid */}
-      {GRID_DATA.map((row, rowIndex) => (
-        <XStack key={rowIndex} w="100%" space="$1" ai="center">
-          {row.map((btn, colIndex) => (
-            <Button key={colIndex} style={[buttonStyle, { backgroundColor: btn.color }]}>
-              <Text>{btn.word}</Text>
-            </Button>
-          ))}
-        </XStack>
-      ))}
-   </YStack>
-  )
+   return (
+      <YStack f={1} jc="flex-start" ai="center" p="$1" space="$1">
+
+         <XStack w="100%" space="$1" jc="space-between" ai="center">
+            <Button style={buttonStyle}><Text>🏠</Text></Button>
+            <Button style={buttonStyle}><Text>🔍</Text></Button> 
+            <Paragraph style={displayStyle}>{displayText}</Paragraph>
+            <Button style={buttonStyle} onPress={deleteLastWord}><Text>⬅️</Text></Button>
+            <Button style={buttonStyle} onPress={clearDisplayText}><Text>X</Text></Button> 
+            <Button style={buttonStyle}><Text>🏠</Text></Button>
+         </XStack>
+
+         {GRID_DATA.map((row, rowIndex) => (
+            <XStack key={rowIndex} w="100%" space="$1" ai="center">
+               {row.map((btn, colIndex) => (
+               
+               <Button 
+                  key={colIndex} 
+                  style={[buttonStyle, { backgroundColor: btn.color }]}
+                  onPress={() => {
+                     if (colIndex >= 1) { 
+                     handleWordButtonPress(btn.word);}}}>
+                 <Text>{btn.word}</Text>
+               </Button>
+             ))}
+            </XStack>
+         ))}
+
+      </YStack>
+   )
 }
